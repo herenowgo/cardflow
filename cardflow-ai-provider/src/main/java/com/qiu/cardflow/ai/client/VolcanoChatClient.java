07db3ae7e059b40e78560b2721529f1682e33997
qiuqiu <your@email.com>
2025-02-19 13:26:48 +0800

    接入火山引擎AI提供商

---
+++ cardflow-ai-provider/src/main/java/com/qiu/cardflow/ai/client/VolcanoChatClient.java
@@ -0,0 +1,71 @@
+package com.qiu.cardflow.ai.client;
+
+import lombok.Getter;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.ai.openai.OpenAiChatOptions;
+import org.springframework.ai.openai.api.OpenAiApi;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+public class VolcanoChatClient implements IChatClient {
+
+    @Value("${spring.ai.volcano.api-key}")
+    private String aiApiToken;
+
+    @Value("${spring.ai.volcano.api-url}")
+    private String aiApiUrl;
+
+    /**
+     * 会有动态代理，不会重复创建实例
+     *
+     * @return
+     */
+    @Bean("VolcanoChatClient1")
+    public ChatClient chatClient() {
+        OpenAiApi openAiApi = new OpenAiApi(aiApiUrl, aiApiToken);
+//        OpenAiApi.builder().completionsPath("");
+        OpenAiChatOptions options = OpenAiChatOptions.builder()
+                .build();
+        ChatModel chatModel = new OpenAiChatModel(openAiApi, options);
+        return ChatClient.builder(chatModel).build();
+    }
+
+
+    @Override
+    public String getName() {
+        return "volcano";
+    }
+
+    @Override
+    public ChatClient getChatClient() {
+        return chatClient();
+    }
+
+    @Override
+    public List<String> getModelVONameList() {
+        return Arrays.stream(AIModel.values())
+                .map(aiModel -> getName() + ":" + aiModel.getName())
+                .toList();
+    }
+
+
+    @Getter
+    public enum AIModel {
+        DEEPSEEK_V3("ep-20250219113402-sqh2k"),
+
+        ;
+
+        private final String name;
+
+        AIModel(String name) {
+            this.name = name;
+        }
+    }
+}
