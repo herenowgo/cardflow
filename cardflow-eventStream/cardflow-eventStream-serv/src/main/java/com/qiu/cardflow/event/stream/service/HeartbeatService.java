4bc91b443d89e1eece07f9eaf6ef333816ac0f0b
qiuqiu <your@email.com>
2025-03-02 16:30:41 +0800

    优化心跳服务代码格式，调整导入顺序，提升可读性

--- cardflow-eventStream/cardflow-eventStream-serv/src/main/java/com/qiu/cardflow/event/stream/service/HeartbeatService.java
+++ cardflow-eventStream/cardflow-eventStream-serv/src/main/java/com/qiu/cardflow/event/stream/service/HeartbeatService.java
@@ -1,15 +1,14 @@
 package com.qiu.cardflow.event.stream.service;
 
-import com.qiu.cardflow.event.stream.model.EventMessage;
-import com.qiu.cardflow.event.stream.model.EventType;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Service;
-
 import java.time.Instant;
 import java.util.HashMap;
 import java.util.Map;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * 心跳服务，定期向所有SSE连接发送心跳消息
@@ -18,7 +17,7 @@ import java.util.Map;
 @Service
 @RequiredArgsConstructor
 public class HeartbeatService {
-    
+
     private final EventStreamService eventStreamService;
 
     /**
@@ -27,11 +26,11 @@ public class HeartbeatService {
     @Scheduled(fixedRate = 30000)
     public void sendHeartbeat() {
         log.debug("Sending heartbeat to all active connections");
-        
+
         Map<String, Object> heartbeatData = new HashMap<>();
         heartbeatData.put("timestamp", Instant.now().toEpochMilli());
         heartbeatData.put("status", "alive");
-        
+
         eventStreamService.broadcastHeartbeat(heartbeatData);
     }
 }
